2de5d301d099bdfaf5577df9e9025b50a71c8d74
Manuel2u <edodoo011@st.ug.edu.gh>
2022-09-07 14:12:49 +0000

    added post, get and delete requests to all articles

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 
-mongoose.connect("mongoDB://localhost:27107/WikiDB", {newUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/WikiDB", {useNewUrlParser: true});
 
 const articleSchema = {
     title: String,
@@ -21,7 +21,43 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 
 
+////////////////// Making Request to all articles ///////////////////////
 
+app.route("/articles")
+.get(function(req, res){
+    Article.find(function(err, foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        }else{
+            res.send(err);
+        }
+    });
+})
+.post(function(req, res){
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save(function(err){
+        if(!err){
+            res.send("Successfully added a new article.");
+        }else{
+            res.send(err);
+        }
+    });
+})
+.delete(function(req, res){
+    Article.deleteMany(function(err){
+        if(!err){
+            res.send("Successfully deleted all articles.");
+        }else{
+            res.send(err);
+        }
+    });
+});
+
+
+////////////////// Making Request to a specific article ///////////////////////
 
 
 
